3d709f1917a0bf12e88f84dd7288b89656b56961
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-07-26 18:56:15 +0200

    NewProductFragment button handlers added

--- app/src/main/java/com/dabyz/greengrocery1/NewProductFragment.kt
+++ app/src/main/java/com/dabyz/greengrocery1/NewProductFragment.kt
@@ -2,8 +2,10 @@ package com.dabyz.greengrocery1
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card_product.view.*
 import kotlinx.android.synthetic.main.fragment_product.*
 
 class NewProductFragment : Fragment(R.layout.fragment_product) {
@@ -11,6 +13,12 @@ class NewProductFragment : Fragment(R.layout.fragment_product) {
         activity?.txFragmentTitle?.text = "Nuevo Producto"
         btnCancel.setOnClickListener {
             activity?.supportFragmentManager?.popBackStack()
+        }
+        btnOk.setOnClickListener{
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        include.imgProduct.setOnClickListener{
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
         }
     }
 
