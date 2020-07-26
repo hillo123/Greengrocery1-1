7770388daa36e47211ebb2ed0715e6275abcaa64
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-07-26 20:21:16 +0200

    button handlers added to EditProductFragment

--- app/src/main/java/com/dabyz/greengrocery1/EditProductFragment.kt
+++ app/src/main/java/com/dabyz/greengrocery1/EditProductFragment.kt
@@ -2,11 +2,23 @@ package com.dabyz.greengrocery1
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card_product.view.*
+import kotlinx.android.synthetic.main.fragment_product.*
 
 class EditProductFragment : Fragment(R.layout.fragment_product) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         activity?.txFragmentTitle?.text = "Edición de Producto"
+        btnCancel.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack()
+        }
+        btnOk.setOnClickListener{
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
+        include.imgProduct.setOnClickListener{
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
+        }
     }
 }
