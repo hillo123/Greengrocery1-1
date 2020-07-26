7d86abbb32475b270ce5a936024357ab8cf46513
Eduardo Medina <eduardo.medina@ext.leroymerlin.es>
2020-07-26 15:26:25 +0200

    cancel button handler added

--- app/src/main/java/com/dabyz/greengrocery1/NewProductFragment.kt
+++ app/src/main/java/com/dabyz/greengrocery1/NewProductFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_product.*
 
 class NewProductFragment : Fragment(R.layout.fragment_product) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         activity?.txFragmentTitle?.text = "Nuevo Producto"
+        btnCancel.setOnClickListener {
+            activity?.supportFragmentManager?.popBackStack()
+        }
     }
 
 }
